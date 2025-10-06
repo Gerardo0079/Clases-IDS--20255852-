9fe3cc44fc633ef610e09205178fd60ca9da1701
Gerardo0079 <20255852@esen.edu.sv>
2025-10-05 21:30:15 -0600

    Clases04

---
+++ Semana4/Clase04-01.py
@@ -0,0 +1,27 @@
+usuario = "Gerardo" #Dato tipo string
+cantidad_alumnos= 74
+media_edad = 18.2312319
+monto_hope = 12311236.97
+inversion_evento = -991934.194921
+
+"""print(type(cantidad_alumnos))
+print(type(media_edad))
+ 
+print(type(cantidad_alumnos) is int) #no es lo mismo int a "int"
+print( type ( media_edad) is int) """
+
+""" print (F"El usuario es {usuario}")
+print (F"y en su aula con {cantidad_alumnos - 4} pajaritos en su aula")
+print (F"con edad promedio de {media_edad:.1f} años")
+print(F"colectaron {monto_hope:,.2f} dolares como un donativo") #Se crea un formato de miles, para las comas
+print (F"y la totalidad de gastos fue de ${abs(inversion_evento):,.2f}") #Para tener formato, muestra el valor absoluto abs
+print(type (usuario) is str)
+
+esta_lloviendo = False
+print (type(esta_lloviendo) is not bool)
+print (type(monto_hope))  """
+
+nombre = "Gerardo "
+apellido = "Jacinto"
+nombre_completo = nombre + "" + apellido
+print(nombre_completo)
---
+++ Semana4/Clase04-02.py
@@ -0,0 +1,8 @@
+cantida_alumnos = 25
+nombre_profe = "Alvin"
+nuevas_inscripciones = 0 
+nombre_profe = input ("Ingres el nombre del profesor: ")
+nuevas_inscripciones = int (input ("Nuevos alumnos: ")) #Input funciona como un string, hay que castearlo para convertilo en otro tipo de dato
+print(type (nombre_profe))
+print (nombre_profe)
+print(cantida_alumnos + nuevas_inscripciones)
