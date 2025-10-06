85b3e259c58d64b2db54de4613332eded7bd5abf
Gerardo0079 <20255852@esen.edu.sv>
2025-10-05 22:45:42 -0600

    Clase04-03

---
+++ Semana4/Clase04-03.py
@@ -0,0 +1,31 @@
+""" Itemingresar = input("Ingresa el item:  ")
+print (F"El item ingresado es {Itemingresar}")
+s1 = ""
+s2 = " "
+s3 = "A B C"
+print (len(s1))
+print (len(s2))
+print (len(s3)) """
+
+my_string = "ABCDEFGHICCC" #string, cadena de texto 
+"""letra = int (input ("Indique el indece de la letra a mostrar (del 0 al 4): "))
+
+print (my_string[0])
+print (my_string[letra])"""
+
+#print (len(my_string))
+# print(my_string[0])
+# print (my_string[1:5:3]) # Hace intervalos, excluye el número final. Luego está el tamaño del paso o salto
+# Para subsetting por slices
+
+#print ("O" not in my_string)
+#print ("A" *8)
+#print (min(my_string))
+#print (max(my_string))
+
+print (my_string.count("C"))
+
+
+
+
+
