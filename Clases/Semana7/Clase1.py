6b7ba4d57b6a3f292fff04d1b811c273a85a4127
Gerardo0079 <20255852@esen.edu.sv>
2025-10-21 18:15:17 -0600

    Hola

---
+++ Clases/Semana7/Clase1.py
@@ -0,0 +1,2 @@
+datos  = [1, 2, "tres", ["lunes", "martes", "miercoles"]]
+print (datos [-1][-1][3])
---
+++ Clases/Semana7/listas.py
@@ -0,0 +1,10 @@
+numeros = ["uno", "dos ", "tres"]
+print (numeros)
+numeros = numeros + ["cuatro", "cinco ", "seis"]
+print (numeros)
+print (len(numeros))
+numeros [2] = "trois"
+print (numeros)
+numeros.append(input ("Escriba el siguiente numero: "))
+numeros.insert (2, "Hola")
+print (numeros)
